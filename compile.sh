2e3a838f8b73533456f91b3a42ed8f6a969c4b90
hhy <552483776@qq.com>
2017-09-21 21:09:22 +0800

    change compile file

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ ./src/server.cpp ./include/*.cpp -o server -g -lrdmacm -libverbs -lpthread -std=gnu99
-g++ ./src/client.cpp ./include/*.cpp -o client -g -lrdmacm -libverbs -lpthread -std=gnu99
+g++ ./src/server.cpp ./include/*.cpp -o server -g -lrdmacm -libverbs -lpthread
+g++ ./src/client.cpp ./include/*.cpp -o client -g -lrdmacm -libverbs -lpthread
