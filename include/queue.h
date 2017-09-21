f95e5bc02a2427b2948a2893d09d971f1dd26ab0
hhy <552483776@qq.com>
2017-09-21 21:28:17 +0800

    change head file

--- include/queue.h
+++ include/queue.h
@@ -21,5 +21,6 @@ void queue_push(Queue *queue, void *buffer);
 void *queue_pop(Queue *queue);
 void queue_destroy(Queue *queue);
 bool queue_if_empty(Queue *queue);
+void queue_push_q(Queue *queue, Queue *q);
 
 #endif
