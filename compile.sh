cce4b16cdd3d70b75c19d614c32edcc254108bd8
hhy <552483776@qq.com>
2017-09-21 15:32:49 +0800

    change to cpp

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc ./src/server.c ./include/*.c -o server -g -lrdmacm -libverbs -lpthread -std=gnu99
-gcc ./src/client.c ./include/*.c -o client -g -lrdmacm -libverbs -lpthread -std=gnu99
+g++ ./src/server.cpp ./include/*.cpp -o server -g -lrdmacm -libverbs -lpthread -std=gnu99
+g++ ./src/client.cpp ./include/*.cpp -o client -g -lrdmacm -libverbs -lpthread -std=gnu99
