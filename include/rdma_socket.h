9442cec28edfb6a626789a85daf3ba854f915836
hhy <552483776@qq.com>
2017-09-23 22:49:30 +0800

    debug client

--- include/rdma_socket.h
+++ include/rdma_socket.h
@@ -86,7 +86,7 @@ void die(char *msg);
 
 Socket* socket_(enum rdma_port_space type);
 void listen_(Socket *socket_, int backlog);
-int connect_(Socket **socket_, char *address, char *port);
+Socket *connect_(Socket *socket_, char *address, char *port);
 Socket *accept_(Socket *socket_, struct Receiver_ *receiver);
 void bind_(Socket *socket_, void *addr, int protocol);
 void close_(Socket *socket_);
