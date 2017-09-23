7064a2988b1416d58e7c413938c7be3199c619f4
hhy <552483776@qq.com>
2017-09-23 21:35:29 +0800

    debug client

--- src/client.cpp
+++ src/client.cpp
@@ -31,6 +31,7 @@ int main(int argc, char **argv) {
 
     for(int i = 0; i < THREAD_NUM; i ++){
         connect = connect_(socket, argv[1], argv[2]);
+        sleep(3);
         pthread_create(p_id + i, NULL, send_process, connect);
     }
     
