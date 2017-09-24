1531b9b3fec21748d3a5076addb282c3cc80243e
hhy <552483776@qq.com>
2017-09-24 13:29:10 +0800

    debug recv flag

--- include/rdma_socket.cpp
+++ include/rdma_socket.cpp
@@ -417,13 +417,13 @@ Socket *recv_(Socket *socket_, Queue *de_queue) {
         }
     }
 
-    if(flag == -1) { 
-        socket_->close_flag = 1;                  // 断开连接会将 close_flag 设成 1
-        return NULL;
-    }
-
     queue_push_q(de_queue, socket_->recv_queue);
     queue_reset(socket_->recv_queue);
+    
+    if(flag == -1) { 
+        socket_->close_flag = 1;                  // 断开连接会将 close_flag 设成 1
+    }
+
     return socket_;
 }
 
